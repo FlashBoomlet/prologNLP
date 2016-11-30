cc4dce461aa8f4e13117f5e2497ed4801b373ce3
Ryan Delao <rdelao@unm.edu>
2016-11-29 19:59:50 -0700

    added recursive support for prepositions)

--- context_sensitive.pl
+++ context_sensitive.pl
@@ -6,10 +6,24 @@ nounphrase([Noun | End], End, Number) :-
     noun(Noun, Number).
 nounphrase([Article, Noun | End], End, Number) :-
     noun(Noun, Number), article(Article, Number).
+nounphrase([Noun | Rest], End, Number) :-
+    noun(Noun, Number),
+    prepositionphrase(Rest, End, _).
+nounphrase([Article, Noun | Rest], End, Number) :-
+    article(Article, Number), noun(Noun, Number),
+    prepositionphrase(Rest, End, _).
 verbphrase([Verb | End], End, Number) :-
     verb(Verb, Number).
-verbphrase([Verb | Rest], End, Number) :-
-    verb(Verb, Number), nounphrase(Rest, End, _).
+verbphrase([Verb, Article | Rest], End, Number) :-
+    verb(Verb, Number), nounphrase([Article | Rest], End, _).
+verbphrase([Verb, Noun | Rest], End, Number) :-
+    verb(Verb, Number), nounphrase([Noun | Rest], End, _).
+verbphrase([Verb, Preposition | Rest], End, Number) :-
+    verb(Verb, Number), prepositionphrase([Preposition | Rest], End, _).
+prepositionphrase([Preposition | Rest], End, Number) :-
+    preposition(Preposition, Number),
+    nounphrase(Rest, End, _).
+
 article(a, singular).
 article(these, plural).
 article(the, singular).
@@ -22,3 +36,4 @@ verb(likes, singular).
 verb(like, plural).
 verb(bites, singular).
 verb(bite, plural).
+preposition(with, _).
